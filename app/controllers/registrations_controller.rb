9d7625f104c6de901c5f82ae7e44b5c3827755e4
LC <eternal.kadir@gmail.com>
2014-11-09 22:57:03 +0000

    style tinkering

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
+    self.resource = User.new resource_params
+    flash.now.alert = "You haven't been invited!"
+    render 'new'
+  end
+
+  private
+  def resource_params
+    params.require('user').permit :email, :password, :password_confirmation, :id
+  end
+
+  def invitation_params
+    params.require('invitation').permit 'hash'
   end
 
 end
