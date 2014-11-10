695ba8d23b6574f49a8bb3daea5cf2c58c283f66
LC <eternal.kadir@gmail.com>
2014-11-10 21:18:42 +0000

    user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+User.create(name:"Pan i Wladca", email: "1@admin.org", password: "adminadmin1", role: "admin")
+User.create(name:"Szary user", email: "1@user.org", password: "useruser1", role: "admin")
