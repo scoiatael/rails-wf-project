259e3afb70ea58da567583f1e9b4b878ae0c75c9
LC <eternal.kadir@gmail.com>
2014-11-16 17:52:25 +0000

    removing votes

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,4 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :vote_options, :reject_if => :all_blank, :allow_destroy => true
 
   private
-  def add_empty_vote
-    self.vote = Vote.create unless self.vote
-  end
 end
--- app/models/vote_option.rb
+++ app/models/vote_option.rb
@@ -1,5 +1,6 @@
 class VoteOption < ActiveRecord::Base
-  belongs_to :vote
   belongs_to :event
   belongs_to :movie
+
+  acts_as_votable
 end
