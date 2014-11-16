d24f7337ed58735a5b5d74092619ee48143c8f85
LC <eternal.kadir@gmail.com>
2014-11-16 17:44:31 +0000

    Act as votable migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116174324) do
+ActiveRecord::Schema.define(version: 20141116174418) do
 
   create_table "events", force: true do |t|
     t.date     "date"
@@ -70,5 +70,20 @@ ActiveRecord::Schema.define(version: 20141116174324) do
 
   add_index "vote_options", ["event_id"], name: "index_vote_options_on_event_id"
   add_index "vote_options", ["movie_id"], name: "index_vote_options_on_movie_id"
+
+  create_table "votes", force: true do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
