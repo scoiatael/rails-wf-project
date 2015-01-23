45ac428738fb5134be8cb37b48fb24934d56b6e9
LC <eternal.kadir@gmail.com>
2015-01-23 22:54:06 +0000

    heroku-mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,15 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.default_url_options = { host: 'https://powerful-river-9464.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
