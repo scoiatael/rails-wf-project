21353b3595f9c19ed1de66e9289a85e9f476d916
LC <eternal.kadir@gmail.com>
2015-01-24 15:22:11 +0000

    more debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.assets.raise_runtime_errors = true
+  config.consider_all_requests_local       = true
   config.action_mailer.default_url_options = { host: 'https://powerful-river-9464.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.gmail.com',
