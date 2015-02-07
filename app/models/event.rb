e21064be90f9c7452991fc0bcba7d8de41d901ee
LC <eternal.kadir@gmail.com>
2015-02-07 20:44:40 +0000

    some vote info for admin

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,6 @@ class Event < ActiveRecord::Base
   has_many :vote_options
   accepts_nested_attributes_for :vote_options, :reject_if => :all_blank, :allow_destroy => true
 
-  def update(argument)
-  end
-
   def vote_open?
     movie.nil?
   end
@@ -17,5 +14,13 @@ class Event < ActiveRecord::Base
   def set_winning_movie
     self.movie = vote_options.max_by(&:sum_of_votes).movie
     self.save!
+  end
+
+  def votes_for
+    vote_options.map(&:votes_for).flatten
+  end
+
+  def user_voted
+    votes_for.map(&:voter_id).uniq
   end
 end
